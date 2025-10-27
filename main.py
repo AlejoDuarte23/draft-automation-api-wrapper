5fbbe08b622755ead44db239a6867ebf6b675db2
AlejoDuarte23 <alejandro7899871776@gmail.com>
2025-10-27 09:54:29 -0500

    feat: first commit

---
+++ main.py
@@ -0,0 +1,159 @@
+import requests
+import os
+from typing import Annotated, Literal, Any
+from dotenv import load_dotenv
+from dsl import RegisterBundleResponse, UploadParameters, GetSignedS3UrlsResponse, CompleteUploadRequest
+
+load_dotenv()
+
+APS_BASE_URL = "https://developer.api.autodesk.com"
+OSS_V2_BASE_URL = f"{APS_BASE_URL}/oss/v2"
+OSS_V4_BASE_URL = f"{APS_BASE_URL}/oss/v4"
+MD_BASE_URL = f"{APS_BASE_URL}/modelderivative/v2" 
+DA_BASE_URL = f"{APS_BASE_URL}/da/us-east/v3" 
+AUTH_URL = f"{APS_BASE_URL}/authentication/v2/token"
+
+SCOPES = "data:read data:write data:create bucket:create bucket:read code:all"
+
+def get_token(client_id: str, client_secret: str) -> str:
+    response = requests.post(
+        AUTH_URL,
+        data={
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "grant_type": "client_credentials",
+            "scope": SCOPES,
+        },
+        timeout=15,
+    )
+    response.raise_for_status()
+    token = response.json()["access_token"]
+    return token
+
+
+def create_bucket(
+    bucketKey: Annotated[str, "Unique Name you assign to a bucket, Possible values: -_.a-z0-9 (between 3-128 characters in length"],
+    token: Annotated[str, "2Lo token"],
+    policy_key: Literal["transient", "temporary", "persistent"]= "transient",
+    access: None | Literal["full", "read"] = "full",
+    region: Literal["US", "EMEA", "AUS", "CAN", "DEU", "IND", "JPN", "GBN"] = "US",
+) -> dict[str, Any]:
+    """
+    Create a bucket in OSS v2.
+    https://aps.autodesk.com/en/docs/data/v2/reference/http/buckets-POST/
+    """
+    url = f"{OSS_V2_BASE_URL}/buckets"
+    payload = {"bucketKey": bucketKey, "access": access, "policyKey": policy_key}
+    headers = {
+        "Authorization":f"Bearer {token}",
+        "Content-Type": "application/json",
+        "x-ads-region": region,
+    }
+    r = requests.post(url, headers=headers, json=payload, timeout=30)
+    r.raise_for_status()
+    return r.json()
+
+def get_signed_s3_upload(
+        bucketKey: Annotated[str, "Unique Name you assign to a bucket, Possible values: -_.a-z0-9 (between 3-128 characters in length"],
+        objectKey: Annotated[str, "URL-encoded object key to create signed URL for, basicallythenameofthefile"],
+        token: Annotated[str, "2Lo Token"]
+)->GetSignedS3UrlsResponse:
+    """
+    We need to check the encoded url part of this!
+    """
+    url = f"{OSS_V2_BASE_URL}/buckets/{bucketKey}/objects/{objectKey}/signeds3upload"
+    headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
+    r = requests.get(url, headers=headers, timeout=30)
+    r.raise_for_status()
+    response = r.json()
+    return GetSignedS3UrlsResponse.model_validate(response)
+
+def put_to_signed_url(signed_url: str, file_path: str) -> int:
+    """
+    Returns HTTP status code, 200 or 201 indicates success
+    """
+    with open(file_path, "rb") as f:
+        r = requests.put(
+            signed_url,
+            data=f,
+            headers={"Content-Type": "application/octet-stream"},
+            timeout=120
+        )
+        r.raise_for_status()
+        return r.status_code
+
+def complete_signed_s3_upload(
+        bucketKey: Annotated[str, "Unique Name of the bocket"],
+        objectKey: Annotated[str, "URL-encoded object key to create signed URL for, basicallythenameofthefile"],
+        uploadKey: Annotated[str, "UploadKey "],
+        token: Annotated[str, "2Lo Token"]
+    ) -> CompleteUploadRequest:
+    url = f"{OSS_V2_BASE_URL}/buckets/{bucketKey}/objects/{objectKey}/signeds3upload"
+    payload = {"uploadKey": uploadKey}
+    header = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
+    r = requests.post(url, headers=header, json=payload, timeout=30)
+    r.raise_for_status()
+    return CompleteUploadRequest.model_validate(**r.json())
+
+def build_oss_urn(
+        bucketKey:Annotated[str, "Unique Name of the bucket"],
+        objectKey: Annotated[str, "URL-encode object key"]
+    ) -> str:
+    return f"urn:adsk.objects:os.object:{bucketKey}/{objectKey}"
+
+
+def register_appbundle(
+        appBundleId: Annotated[str, "Name of AppBundle Only alphanumeric characters and _ (underscore) are allowed."],
+        engine: Annotated[str, "Engine to be use in the Automation api e.g'Autodesk.Revit+2021'"],
+        description: Annotated[str, "App bundle description"],
+        token: Annotated[str, "2Lo Token"]
+)->RegisterBundleResponse:
+    url = f"{DA_BASE_URL}/appbundles" 
+    payload = {"id": appBundleId, "engine": engine, "description":description}
+    header = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
+    r = requests.post(url,headers=header, json=payload)
+    r.raise_for_status()
+    return RegisterBundleResponse(**r.json())
+
+def upload_appbundle(upload_parameters: UploadParameters, zip_path: str) -> Annotated[int, "Status Code e.g 200"]:
+    with open(zip_path, "rb") as f:
+        files = {**upload_parameters.formData.model_dump(by_alias=True, exclude_none=True), 'file': (os.path.basename(zip_path), f, "application/octet-stream")}
+        r = requests.post(upload_parameters.endpointURL, files=files, timeout=60)
+    r.raise_for_status()
+    return r.status_code
+
+def delete_appbundle(appbundleId:str, token: str) -> ...:
+    """
+    If return 204 then ok
+    """
+    url = f"{DA_BASE_URL}/appbundles/{appbundleId}"
+    header = {"Authorization": f"Bearer {token}"}
+    r = requests.delete(url=url, headers=header)
+    r.raise_for_status() 
+    return r.status_code
+
+def get_signed_s3_download(
+        bucketKey: Annotated[str, "Unique name of the bucket"],
+        objectKey: Annotated[str, "URL-encoded object key to create signed URL for, basicallythenameofthefile"],
+        token: Annotated[str, "2Lo Token"]
+) -> None:
+    url = f"{OSS_V2_BASE_URL}/buckets/{bucketKey}/objects/{objectKey}/signeds3download"
+    header = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
+    r = requests.get(url=url, headers=header, timeout=30)
+    r.raise_for_status()
+    return r.json()
+
+def dowload_from_signed_url(
+        signed_url: Annotated[str, "Signed url from the previos"],
+        output_path: Annotated[str, "Output path str"],
+)-> int:
+    """
+    """
+    r = requests.get(signed_url, timeout=120)
+    r.raise_for_status()
+    
+    with open(output_path, "wb") as f:
+        f.write(r.content)
+    
+    return r.status_code
+        
---
+++ tests.py
@@ -0,0 +1,63 @@
+import os
+import logging
+from pathlib import Path
+from dotenv import load_dotenv
+from main import get_token, create_bucket, register_appbundle, delete_appbundle, upload_appbundle, get_signed_s3_upload, put_to_signed_url, complete_signed_s3_upload
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("CLIENT_ID", "")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET", "")
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
+
+def test_upload_register_appbundle(token:str):
+    app_bundle_name = "mytestbundle"
+    try:
+        register_response = register_appbundle(
+            appBundleId=app_bundle_name, engine="Autodesk.Revit+2024",token=token, description="test register")
+        zip_path=Path(__file__).parent / "bundles" / "IfcExportDA.bundle.zip"
+        status_code = upload_appbundle(register_response.uploadParameters, zip_path=str(zip_path))
+        print(status_code)
+        
+    except Exception as e:
+        logging.warning(f"[ERROR] {e}")
+
+    r = delete_appbundle(app_bundle_name, token=token)
+
+def test_upload_and_download_to_bucket(token: str) -> None: 
+    bucketKey = "natalisbucket4"
+    objectKey = "wombat4.rvt"
+
+    # 1. Path to the file
+    zip_path=Path(__file__).parent / "bundles" / "SteelPortalFrame.rvt"
+    
+    #2. Create bucket
+    try:
+        bucket_response = create_bucket(bucketKey=bucketKey, token=token)
+    except Exception:
+        pass
+    
+    #3. Generate s3 signed urls
+    signed_url_response = get_signed_s3_upload(bucketKey=bucketKey, objectKey=objectKey, token=token)
+    upload_key = signed_url_response.uploadKey
+    signed_url = signed_url_response.urls[0]
+    print(f"{signed_url=}")
+    
+    #4. Upload to signed s3 key
+    status = put_to_signed_url(signed_url=signed_url, file_path=str(zip_path))
+    
+    # 5. Confirm Upload
+    response = complete_signed_s3_upload(bucketKey=bucketKey, objectKey=objectKey, uploadKey=upload_key, token=token)
+    print(response)
+
+
+
+
+
+
+if __name__ == "__main__":
+    token = get_token(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+    print(token)
+    #test_upload_register_appbundle(token=token)
+    test_upload_and_download_to_bucket(token=token)
